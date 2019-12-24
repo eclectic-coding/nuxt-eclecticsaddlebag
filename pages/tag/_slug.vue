6243604d8f4fc5720f25eb69b0c40610033cf3bf
eclectic-coding <chuck@eclecticsaddlebag.com>
2019-12-24 12:39:20 -0500

    Debug tag filtering.

--- pages/tag/_slug.vue
+++ pages/tag/_slug.vue
@@ -1,32 +1,26 @@
 <template>
   <div>
-    <h1 :key="'/' + params.slug">Posts:</h1>
+    <h1 :key="$route.params.slug">Posts:</h1>
     <ul>
       <li v-for="post in posts" :key="post.attributes.title">
-        <nuxt-link :to="'/tag'">
-          {{post.attributes.title}}
-        </nuxt-link>
+        <PostShort :post="post" />
       </li>
     </ul>
   </div>
 </template>
 <script>
+  import PostShort from '../../components/PostShort'
+
   export default {
-    async asyncData() {
+    components: { PostShort },
+    async asyncData({ params }) {
       const resolve = await require.context('~/content/posts/', true, /\.md$/)
       let imports = resolve.keys().map((key) => resolve(key))
-
+      // filter out page type
       imports = imports.filter(
-        (post) =>
-          post.attributes.tags.filter((tag) => tag === post.attributes.slug).length
+        (post) => post.attributes.tags.filter((tag) => tag === params.slug).length
       )
       return { posts: imports }
-    },
-    props: {
-      post: {
-        type: Object,
-        default: () => {}
-      }
     }
   }
 </script>
